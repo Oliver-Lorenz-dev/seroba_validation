78566aee1c43f09f7ae59c2c3031401e03f56bbe
Oliver-Lorenz-dev <oliverlorenz.business@gmail.com>
2024-09-02 08:41:26 +0100

    fix 06A-IV test

--- 06A-IV/validate.py
+++ 06A-IV/validate.py
@@ -5,12 +5,13 @@ fasta_file = sys.argv[1]
 fasta_sequences = SeqIO.parse(open(fasta_file),'fasta')
 gene_position = sys.argv[2]
 
-mutation_pos_list = [485]
+mutation_pos_list = [487]
 mutation_list = ['T']
 
 for sequence in fasta_sequences:
     for i in range(0, len(mutation_pos_list)):
-        codon = sequence.seq[mutation_pos_list[i]]
+        pos = int(gene_position) + int(mutation_pos_list[i] - 2)
+        codon = sequence.seq[(pos + mutation_pos_list[i])]
         if codon != mutation_list[i]:
             raise SystemExit(f"Test failed: {codon} at position {mutation_pos_list[i]} is not equal to {mutation_list[i]}")
 
